6b5360f10375cbf906df3d5cce946a8aa8f0929f
Joshua Howland <development@jkhowland.com>
2015-06-09 11:47:38 -0700

    Fixes typo in notes

--- PodiumKit/Notes.swift
+++ PodiumKit/Notes.swift
@@ -2,10 +2,9 @@
 
 App functions on Core Data and Fetched Result Controllers
 
-
-
-Friend list is downloaded and run out of a UserController
+Friend list is downloaded and run out of a FriendController
 
 Invites and Friends are created locally before being pushed, and simply marked as failed to upload if failed
+
 
 */
